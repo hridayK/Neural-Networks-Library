377d320c69d135dc950d2d4fa18c756f0e1f4834
JK <86554470+Jyotikakakar@users.noreply.github.com>
2023-02-06 08:30:19 +0530

    Activation functions

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from nnl import layers
+from nnl import layers,activation
 import numpy as np
 
 
@@ -7,9 +7,19 @@ inputs = np.array([[1.2,3.9],
                 [8.3,5.4]])
 
 layer1 = layers.dense(2,10)
+activation1 = activation.relu()
 layer2 = layers.dense(10,3)
+activation2 = activation.softmax()
 
-layer1.forward(inputs=inputs)
-layer2.forward(inputs=layer1.output)
+layer1.forward(inputs)
+activation1.forward(layer1.output)
+layer2.forward(activation1.output)
+activation2.forward(layer2.output)
 
+print(layer1.output)
+print('\nOutput after applying ReLu function:\n')
+print(activation1.output)
+print()
 print(layer2.output)
+print('\nOutput after applying Softmax function:\n')
+print(activation2.output)
