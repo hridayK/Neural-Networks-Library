159c37d50ceebc44641937013a4a36358a1c4a02
Hriday Keswani <hridaykeswani@gmail.com>
2023-02-06 09:21:21 +0530

    implement elu and sigmoid function, modify and add documentation and update README.md

--- README.md
+++ README.md
@@ -1,9 +1,9 @@
 # Neural-Networks-Library
 A python library for making simple neural networks built with numpy
 
-## Example of forward propogation:
+## Example of using NNL:
 ```python
-from nnl import layers
+from nnl import layers,activation
 import numpy as np
 
 
@@ -12,10 +12,20 @@ inputs = np.array([[1.2,3.9],
                 [8.3,5.4]])
 
 layer1 = layers.dense(2,10)
+activation1 = activation.relu()
 layer2 = layers.dense(10,3)
+activation2 = activation.softmax()
 
-layer1.forward(inputs=inputs)
-layer2.forward(inputs=layer1.output)
+layer1.forward(inputs)
+activation1.forward(layer1.output)
+layer2.forward(activation1.output)
+activation2.forward(layer2.output)
 
+print(layer1.output)
+print('\nOutput after applying ReLu function:\n')
+print(activation1.output)
+print()
 print(layer2.output)
+print('\nOutput after applying Softmax function:\n')
+print(activation2.output)
 ```
--- nnl/activation.py
+++ nnl/activation.py
@@ -1,14 +1,25 @@
+"""
+Activation functions
+--------------------
+List of available activation functions:
+- Softmax
+- ReLu
+- ELU
+- Sigmoid
+"""
+
 import numpy as np
+
 class softmax:
     """
-    Softmax functions: gives normalized values as output:\n
+    Softmax functions: converts a vector into probability distribution as output:\n
     ``self.output = e^zi / Σ (e^z)``\n
     where:
     zi = element of a batch z at index i
     """
     def forward(self, inputs:np.ndarray):
         """
-        ```self.output``` get the values that are the output of the softmax function.
+        ```self.output``` gets values that are output of the Softmax function.
         """
         exp_values = np.exp(inputs - np.max(inputs, axis=1, keepdims=True))
         probabilities = exp_values / np.sum(exp_values, axis=1, keepdims=True)
@@ -16,11 +27,42 @@ class softmax:
 
 class relu:
     """
-    ReLu: Rectied Linear Activation Unit
+    ReLu: Rectied Linear Activation Unit\n
+    Applies ReLu function giving the output as:
         ```max(0,inputs)```
     """
     def forward(self, inputs:np.ndarray):
         """
-        ```self.output``` get the values that are the output of the softmax function.
+        ```self.output``` gets values that are output of the ReLu function.
         """
         self.output = np.maximum(0,inputs)
+
+class elu:
+    """
+    ELU: Exponential Linear Unit
+    It is a modified form of ReLu function giving output as:
+    ```
+    def elu(x):
+        output = x if x > 0
+        output = α * (e^x - 1) if x <= 0
+    ```
+    alpha(α) has default value as 1.0.
+    """
+    def forward(self, inputs:np.ndarray, alpha=1.0):
+        """
+        ```self.output``` gets values that are output of the ELU function.
+        """
+        self.output = np.where(input>0, inputs, alpha*(np.exp(inputs) - 1))
+
+class sigmoid:
+    """
+    Sigmoid activation function gives outputs as:
+    ```
+        sigmoid(x) = 1 / (1 + exp(-x))
+    ```
+    """
+    def forward(self, inputs):
+        """
+        ``self.output`` gets values that are output of the Sigmoid function.
+        """
+        self.output = 1 / (1 + np.exp(-1*inputs))
