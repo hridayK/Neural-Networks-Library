158c9ff698e61f5049d011984132875eddfa0322
JK <86554470+Jyotikakakar@users.noreply.github.com>
2023-04-16 15:17:17 +0530

    optimizers

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from nnl import bundle as nn
-# import numpy as np
+from nnl import optimizers
+import numpy as np
 from nnfs.datasets import spiral_data
 
 X, y = spiral_data(samples=100, classes=3)
@@ -14,6 +15,9 @@ activation1.forward(layer1.output)
 layer2.forward(activation1.output)
 loss = loss_activation.forward(layer2.output,y)
 
+print("weights shape after forward: ", layer1.weights.shape)
+print("bias shape after forward: ", layer1.biases.shape)
+
 loss_function = nn.categorical_cross_entropy()
 
 print("Loss: ", loss)
@@ -22,6 +26,21 @@ loss_activation.backward(loss_activation.output, y)
 layer2.backward(loss_activation.dinputs)
 activation1.backward(layer2.dinputs)
 layer1.backward(activation1.dinputs)
+
+print("weights shape after backward: ", layer1.dweights.shape)
+print("bias shape after backward: ", layer1.dbiases.shape)
+
+print(f"\nlayer 1:\n\ndweights = {layer1.dweights}\n\ndbiases = {layer1.dbiases}\n")
+print(f"\nlayer 2:\n\ndweights = {layer2.dweights}\n\ndbiases = {layer2.dbiases}")
+
+optimizer = optimizers.AdamOptimizer(learning_rate=0.01, beta1=0.9, beta2=0.999, epsilon=1e-8)
+
+print("weights shape: ", layer2.dweights.shape)
+
+layer1.dweights += optimizer.optimize(layer1.dweights)
+layer1.dbiases += optimizer.optimize(layer1.dbiases)
+layer2.dweights += optimizer.optimize(layer2.dweights)
+layer2.dbiases += optimizer.optimize(layer2.dbiases)
 
 print(f"\nlayer 1:\n\ndweights = {layer1.dweights}\n\ndbiases = {layer1.dbiases}\n")
 print(f"\nlayer 2:\n\ndweights = {layer2.dweights}\n\ndbiases = {layer2.dbiases}")
