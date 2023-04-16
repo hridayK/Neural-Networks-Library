92428d228bddd59a373b2ac05471cbd7e71063ab
JK <86554470+Jyotikakakar@users.noreply.github.com>
2023-04-16 17:43:56 +0530

    layers

--- main.py
+++ main.py
@@ -59,12 +59,16 @@ from nnfs.datasets import spiral_data
 
 X, y = spiral_data(samples=100, classes=3)
 
-layer1 = nn.dense(2, 3)
+layer1 = nn.dense(2, 64)
 activation1 = nn.relu()
-layer2 = nn.dense(3, 3)
+layer2 = nn.dense(64, 64)
+activation2 = nn.relu()
+layer3 = nn.dense(64, 64)
+activation3 = nn.relu()
+layer4 = nn.dense(64, 3)
 loss_activation = nn.activation_softmax_loss_activation_category()
 
-optimizer = optimizers.AdamOptimizer(learning_rate=0.01, beta1=0.9, beta2=0.999, epsilon=1e-8)
+optimizer = optimizers.AdamOptimizer(learning_rate=0.001, beta1=0.9, beta2=0.999, epsilon=1e-8)
 
 # Train model
 for epoch in range(1000):
@@ -72,7 +76,11 @@ for epoch in range(1000):
     layer1.forward(X)
     activation1.forward(layer1.output)
     layer2.forward(activation1.output)
-    loss = loss_activation.forward(layer2.output, y)
+    activation2.forward(layer2.output)
+    layer3.forward(activation2.output)
+    activation3.forward(layer3.output)
+    layer4.forward(activation3.output)
+    loss = loss_activation.forward(layer4.output, y)
     
     # Print loss every 100 epochs
     if epoch % 100 == 0:
@@ -80,7 +88,11 @@ for epoch in range(1000):
     
     # Backward pass
     loss_activation.backward(loss_activation.output, y)
-    layer2.backward(loss_activation.dinputs)
+    layer4.backward(loss_activation.dinputs)
+    activation3.backward(layer4.dinputs)
+    layer3.backward(activation3.dinputs)
+    activation2.backward(layer3.dinputs)
+    layer2.backward(activation2.dinputs)
     activation1.backward(layer2.dinputs)
     layer1.backward(activation1.dinputs)
     
@@ -89,12 +101,20 @@ for epoch in range(1000):
     layer1.biases += optimizer.optimize(layer1.dbiases)
     layer2.weights += optimizer.optimize(layer2.dweights)
     layer2.biases += optimizer.optimize(layer2.dbiases)
+    layer3.weights += optimizer.optimize(layer3.dweights)
+    layer3.biases += optimizer.optimize(layer3.dbiases)
+    layer4.weights += optimizer.optimize(layer4.dweights)
+    layer4.biases += optimizer.optimize(layer4.dbiases)
 
 # Evaluate model
 layer1.forward(X)
 activation1.forward(layer1.output)
 layer2.forward(activation1.output)
-predictions = np.argmax(layer2.output, axis=1)
+activation2.forward(layer2.output)
+layer3.forward(activation2.output)
+activation3.forward(layer3.output)
+layer4.forward(activation3.output)
+predictions = np.argmax(layer4.output, axis=1)
 accuracy = np.mean(predictions == y)
 
 print(f"Accuracy: {accuracy:.4f}")
